8dfab09ebf135078a00684029df773943224c28d
Daniel Kay <contact@danielkay.co.uk>
2014-12-31 21:17:05 +0000

    changed production DB env vars

--- config/env/production.js
+++ config/env/production.js
@@ -1,7 +1,13 @@
 'use strict';
 
 module.exports = {
-    db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean',
+    db: {
+        uri: process.env.MONGODB_ADDON_HOST + '/' + MONGODB_ADDON_DB,
+        options: {
+            user: MONGODB_ADDON_USER,
+            pass: MONGODB_ADDON_PASSWORD
+        }
+    }
     facebook: {
         clientID: process.env.FACEBOOK_ID || 'APP_ID',
         clientSecret: process.env.FACEBOOK_SECRET || 'APP_SECRET',
