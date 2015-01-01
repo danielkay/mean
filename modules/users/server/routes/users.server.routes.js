7fb114f53d4d2c32bedeb379e0fc353dcfefb876
Daniel Kay <contact@danielkay.co.uk>
2015-01-01 05:20:50 +0000

    added users/gravatar route

--- modules/users/server/routes/users.server.routes.js
+++ modules/users/server/routes/users.server.routes.js
@@ -15,6 +15,7 @@ module.exports = function(app) {
 	app.route('/api/users/accounts').delete(users.removeOAuthProvider);
 	app.route('/api/users/password').post(users.changePassword);
 	app.route('/api/users/picture').post(users.changeProfilePicture);
+	app.route('/api/users/gravatar').post(users.gravatarProfilePicture);
 
 	// Finish by binding the user middleware
 	app.param('userId', users.userByID);
